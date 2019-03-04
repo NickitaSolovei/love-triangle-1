6ad575c4b63634d7815b5f8613317dfaf2707f2f
Nickita Solovei <https://github.com/NickitaSolovei>
2019-03-04 11:35:14 +0200

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -4,4 +4,20 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var shech = 0;
+
+  for (let i = 0; i < preferences.length; i++) {
+    const firstPers = i;
+    const secondPers = preferences[i] - 1;
+    if (firstPers == secondPers){
+      continue;
+    }
+    const thirdPers = preferences[secondPers] - 1;
+    
+    if (firstPers == (preferences[thirdPers] - 1)) {
+      shech++;
+    }
+  }
+  
+  return shech / 3;
 };
